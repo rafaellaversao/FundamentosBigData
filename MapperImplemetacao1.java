02a6ee7097a9d5f86fcfb02859e65a8a09411f6b
Rafaella Versão <83784295+rafaellaversao@users.noreply.github.com>
2023-06-28 11:34:12 -0300

    Add files via upload

---
+++ MapperImplemetacao1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pucpr.implementacao;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ *
+ * @author Rafaella
+ */
+class MapperImplemetacao1 extends Mapper {
+    
+}
